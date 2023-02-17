c08b5f96317c27fc1d3ecea0b1ebce203c026467
Srujanmukund24 <srujanmukund10@gmail.com>
2023-02-17 19:45:33 +0530

    navbar added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+     <nav class="navbar fixed-top navbar-expand-lg navbar-dark shadow p-2 mb-5 ">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Cultivo</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Our Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact-Us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    </>
   )
 }
 
