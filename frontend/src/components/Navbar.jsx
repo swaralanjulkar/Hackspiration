e95f494aec51d96ca86f32802f92b6bfefdbe01f
Jayesh <jayeshrpandav@gmail.com>
2023-02-18 14:19:57 +0530

    imported css in all jsx files

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,32 +1,48 @@
-import React from 'react'
+import React from "react";
+import'./styles/navbar.css'
 
 const Navbar = () => {
   return (
     <>
-     <nav className="navbar fixed-top navbar-expand-lg navbar-dark shadow p-2 mb-5 ">
+      <nav className="navbar fixed-top navbar-expand-lg navbar-dark shadow p-2 mb-5 ">
         <div className="container-fluid">
-            <a className="navbar-brand" href="#">Cultivo</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Our Services</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Contact-Us</a>
-                    </li>
-                </ul>
-            </div>
+          <a className="navbar-brand" href="#">
+            Cultivo
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Our Services
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Contact-Us
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-    </nav>
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- frontend/src/components/WeatherWidget.jsx
+++ frontend/src/components/WeatherWidget.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import'./styles/weather.css'
 
 const WeatherWidget = () => {
   return (
