d4006f8ed99117486b45269991003dca78ced860
ayushchopade45 <112979270+ayushchopade45@users.noreply.github.com>
2023-02-25 00:12:57 +0530

    Delete server directory

--- server/createdb.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('database.db')
-cursor = conn.cursor()
-cursor.execute('''CREATE TABLE my_table
-                  (name TEXT PRIMARY KEY,
-                   description TEXT)''')
-conn.commit()
-conn.close()
